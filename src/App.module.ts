262e7e9dcdda71c0f8de1795ce1e4bdda6842cd5
Do Tran Thai An <justapie06@gmail.com>
2026-05-08 21:53:20 +0700

    feat: integrate typeorm

--- src/App.module.ts
+++ src/App.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
-    
+    TypeOrmModule.forRoot({
+      type: "better-sqlite3",
+      database: "data.db",
+      autoLoadEntities: true,
+      cache: true,
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
